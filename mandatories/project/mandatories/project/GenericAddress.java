a04bf89be17f7a23a46f5402d7c2c13e1755b79e
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-19 08:08:44 -0500

    Create GenericAddress.java file

---
+++ mandatories/project/mandatories/project/GenericAddress.java
@@ -0,0 +1,10 @@
+package mandatories.project;
+
+public class GenericAddress {
+
+ public static Address parse(String address) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'parse'");
+ }
+
+}
