7095091d9de1962929fd3d4eab36cbb84e6653d0
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-19 08:11:53 -0500

    Create ProjectGet.java file

---
+++ mandatories/project/mandatories/project/ProjectGet.java
@@ -0,0 +1,76 @@
+package mandatories.project;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class ProjectGet {
+ public static final int DEFAULT_VERSION = ProjectConstants.version2c;
+ public static final int DEFAULT_PORT = 161;
+ public static final long DEFAULT_TIMEOUT = 1500;
+ public static final int DEFAULT_RETRIES = 2;
+ public static final String DEFAULT_PROTOCOL = "udp";
+
+ public static CommunitiyTarget getTarget(String address, String community) {
+  String ip = "192.168.0.1"; // Replace with the desired IP address
+  Address targetAddress = GenericAddress.parse(DEFAULT_PROTOCOL + ";" + ip + "/" + DEFAULT_PORT);
+  CommunitiyTarget target = new CommunitiyTarget();
+  target.setCommunity(new OctetString());
+  target.setAddress(targetAddress);
+  target.setRetries(DEFAULT_RETRIES);
+  target.setTimeout(DEFAULT_TIMEOUT);
+  target.setVersion(DEFAULT_VERSION);
+  return target;
+ }
+
+ public static PDU projectGet(String ip, String community, String oid) throws IOException {
+  Project project = null;
+  PDU response = null;
+  try {
+   // ...
+
+   PDU pdu = new PDU();
+   pdu.add(new VariableBinding());
+
+   // ...
+  } catch (Exception e) {
+   e.printStackTrace();
+   System.out.println("Error: " + e.getMessage());
+  } finally {
+   if (project != null) {
+    ((Closeable) project).close();
+   }
+  }
+  return response;
+ }
+
+ private static CommunityTarget createDefault(String ip, String community) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'createDefault'");
+ }
+
+ public static long getPDUvalue(PDU response) {
+  long IfInOctets = -1;
+  for (int i = 0; i < response.size(); i++) {
+   VariableBinding vb = response.get(i);
+   String value = vb.getVariable().toString();
+   if (!value.equals("Null")) {
+    IfInOctets = Long.parseLong(value);
+   } else {
+    IfInOctets = NULL_DATA_RECIEVED;
+   }
+  }
+  return IfInOctets;
+ }
+
+ public static final int NULL_DATA_RECIEVED = -10;
+
+ public static String getPDUStringValue(PDU response) {
+  String response_str = "";
+  for (int i = 0; i < response.size(); i++) {
+   VariableBinding vb = response.get(i);
+   response_str = vb.getVariable().toString();
+  }
+  return response_str;
+ }
+}
+// Path: mandatories/project/mandatories/project/ProjectConstants.java
