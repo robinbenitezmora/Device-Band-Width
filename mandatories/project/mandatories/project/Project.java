a10417a0befe975b7bbba606b474af750a610666
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-20 07:17:40 -0500

    Create Project.java file

--- mandatories/project/mandatories/project/Project.java
+++ mandatories/project/mandatories/project/Project.java
@@ -2,4 +2,24 @@ package mandatories.project;
 
 public class Project {
 
+ public void listen() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'listen'");
+ }
+
+ public void cancel(Object request, ResponseListener responseListener) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'cancel'");
+ }
+
+ public void send(PDU pdu, CommunitiyTarget target, Object object, ResponseListener listener) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'send'");
+ }
+
+ public void close() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'close'");
+ }
+
 }
