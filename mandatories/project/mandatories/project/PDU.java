637ac65d5f5ceda93771303da73fb064552a9766
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-20 07:17:20 -0500

    Create PDU.java file

--- mandatories/project/mandatories/project/PDU.java
+++ mandatories/project/mandatories/project/PDU.java
@@ -2,6 +2,8 @@ package mandatories.project;
 
 public class PDU {
 
+ public static final String GET = null;
+
  public void add(VariableBinding variableBinding) {
   // TODO Auto-generated method stub
   throw new UnsupportedOperationException("Unimplemented method 'add'");
@@ -15,6 +17,16 @@ public class PDU {
  public VariableBinding get(int i) {
   // TODO Auto-generated method stub
   throw new UnsupportedOperationException("Unimplemented method 'get'");
+ }
+
+ public int getErrorStatus() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getErrorStatus'");
+ }
+
+ public void setType(String get2) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setType'");
  }
 
 }
