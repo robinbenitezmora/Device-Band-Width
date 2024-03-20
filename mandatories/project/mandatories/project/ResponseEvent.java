1313e5aec03ba422bb9f9f822dd7d0b0403001df
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-20 07:18:04 -0500

    Refactor ResponseEvent.java file

--- mandatories/project/mandatories/project/ResponseEvent.java
+++ mandatories/project/mandatories/project/ResponseEvent.java
@@ -2,4 +2,19 @@ package mandatories.project;
 
 public class ResponseEvent {
 
+ public Project getSource() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getSource'");
+ }
+
+ public Object getRequest() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getRequest'");
+ }
+
+ public PDU getResponse() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getResponse'");
+ }
+
 }
