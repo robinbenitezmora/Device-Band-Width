503dfe120ee41092f7f97347d3705b8e9ed645ca
robinbenitezmora <robinbenitez1990@gmail.com>
2024-03-27 15:04:38 -0500

    Create ResponseEvent.java class

--- mandatories/project/mandatories/project/ResponseEvent.java
+++ mandatories/project/mandatories/project/ResponseEvent.java
@@ -12,9 +12,14 @@ public class ResponseEvent {
   throw new UnsupportedOperationException("Unimplemented method 'getRequest'");
  }
 
- public PDU getResponse() {
+ public static PDU getResponse() {
   // TODO Auto-generated method stub
   throw new UnsupportedOperationException("Unimplemented method 'getResponse'");
+ }
+
+ public static String getPeerAddress() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getPeerAddress'");
  }
 
 }
