b2229265233ae13c4add09b448621ea78d7c4b90
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-01 08:09:52 -0500

    Refactor Address.java file

--- mandatories/project/mandatories/project/Address.java
+++ mandatories/project/mandatories/project/Address.java
@@ -2,4 +2,14 @@ package mandatories.project;
 
 public class Address {
 
+ public Object getInetAddress() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getInetAddress'");
+ }
+
+ public Object getPort() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getPort'");
+ }
+
 }
