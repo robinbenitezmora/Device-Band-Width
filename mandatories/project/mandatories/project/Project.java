49f28ae3fef441c834e24e9891a75d8f8abfb154
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-01 08:16:22 -0500

    Refactor Project.java file

--- mandatories/project/mandatories/project/Project.java
+++ mandatories/project/mandatories/project/Project.java
@@ -3,58 +3,36 @@ package mandatories.project;
 public class Project {
 
  public void listen() {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'listen'");
  }
 
  public void cancel(Object request, ResponseListener responseListener) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'cancel'");
  }
 
  public ResponseEvent send(PDU pdu, CommunitiyTarget target, Object object, ResponseListener listener) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'send'");
+  return null;
  }
 
  public void close() {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'close'");
  }
 
  public Object getMessageDispatcher() {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'getMessageDispatcher'");
+  return null;
  }
 
  public void addCommandResponder(ProjectTrapMultiThreadReceiver projectTrapMultiThreadReceiver) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'addCommandResponder'");
- }
-
- public void addMessageProcessingModel(MPv1 mPv1) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
  }
 
  public void addMessageProcessingModel(MPv2c mPv2c) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
  }
 
  public void addMessageProcessingModel(MPv1 mPv1) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
  }
 
  public void addMessageProcessingModel(MPv3 mPv3) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
  }
 
  public void addSecurityModel(USM usm) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'addSecurityModel'");
+
  }
 
 }
