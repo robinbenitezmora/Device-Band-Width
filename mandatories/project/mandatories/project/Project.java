01e157450dae5d8ca3c93638eb91cb00ab0437ad
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-01 08:10:08 -0500

    Refactor Project.java file

--- mandatories/project/mandatories/project/Project.java
+++ mandatories/project/mandatories/project/Project.java
@@ -32,4 +32,29 @@ public class Project {
   throw new UnsupportedOperationException("Unimplemented method 'addCommandResponder'");
  }
 
+ public void addMessageProcessingModel(MPv1 mPv1) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
+ }
+
+ public void addMessageProcessingModel(MPv2c mPv2c) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
+ }
+
+ public void addMessageProcessingModel(MPv1 mPv1) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
+ }
+
+ public void addMessageProcessingModel(MPv3 mPv3) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
+ }
+
+ public void addSecurityModel(USM usm) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addSecurityModel'");
+ }
+
 }
