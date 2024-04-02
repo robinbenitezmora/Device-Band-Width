47fbbecdbeee2920c6bbba26d17c33717107e0f6
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-02 08:00:37 -0500

    Refactor VariableBinding.java file

--- mandatories/project/mandatories/project/VariableBinding.java
+++ mandatories/project/mandatories/project/VariableBinding.java
@@ -2,6 +2,10 @@ package mandatories.project;
 
 public class VariableBinding {
 
+ public VariableBinding(OID targetOID) {
+  // TODO Auto-generated constructor stub
+ }
+
  public Object getVariable() {
   // TODO Auto-generated method stub
   throw new UnsupportedOperationException("Unimplemented method 'getVariable'");
