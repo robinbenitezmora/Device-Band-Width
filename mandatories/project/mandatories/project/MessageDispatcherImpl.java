12c0fe382091ca23411d66fa6eb5dc0f2762bc5a
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-02 07:55:58 -0500

    Refactor MessageDispatcherImpl.java file

--- mandatories/project/mandatories/project/MessageDispatcherImpl.java
+++ mandatories/project/mandatories/project/MessageDispatcherImpl.java
@@ -2,4 +2,19 @@ package mandatories.project;
 
 public class MessageDispatcherImpl {
 
+ public void addMessageProcessingModel(MPv1 mPv1) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
+ }
+
+ public void addMessageProcessingModel(MPv2c mPv2c) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
+ }
+
+ public void addMessageProcessingModel(MPv3 mPv3) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'addMessageProcessingModel'");
+ }
+
 }
