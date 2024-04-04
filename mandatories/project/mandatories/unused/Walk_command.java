d2238755e3dd20b9a1daa9f2040776d93c4da4f7
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-04 08:18:21 -0500

    Create Walk_command.java class

---
+++ mandatories/project/mandatories/unused/Walk_command.java
@@ -0,0 +1,12 @@
+package mandatories.unused;
+
+import java.util.ArrayList;
+
+public class Walk_command {
+
+ public static ArrayList<String> projectWalk(String ip1, String community, String oid) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'projectWalk'");
+ }
+
+}
