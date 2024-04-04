36b201acb9f3891c90d590279a8d3254a659c9e6
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-04 08:15:38 -0500

    Create DataProcessor.java class

---
+++ mandatories/project/mandatories/unused/DataProcessor.java
@@ -0,0 +1,25 @@
+package mandatories.unused;
+
+public class DataProcessor {
+
+ public void show_raw_data(Object data_collected) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'show_raw_data'");
+ }
+
+ public void prepare_data(Object data_collected) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'prepare_data'");
+ }
+
+ public String[][] get_database_data() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'get_database_data'");
+ }
+
+ public void show_processed_data() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'show_processed_data'");
+ }
+
+}
